64e203bfa70e392abec9725eec724abce14fe693
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-11 13:20:10 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import localFont from 'next/font/local'
+ 
+const satoshi = localFont({ src: './Satoshi-Variable.woff2' })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${satoshi.className} antialiased`}
       >
         {children}
       </body>
