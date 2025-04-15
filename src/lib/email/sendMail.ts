0957adddd35dbbed7eb2fab556856c7500a10b90
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-15 18:09:04 +0200

    ADD: Implement email sending functionality with nodemailer

---
+++ src/lib/email/sendMail.ts
@@ -0,0 +1,18 @@
+import { transporter } from "./transporter";
+
+export async function sendMail ({
+    to,
+    subject,
+    message
+}: {
+    to: string;
+    subject: string;
+    message: string;}) {
+        // Send message using the transporter
+        await transporter.sendMail({
+            from: process.env.EMAIL,
+            to,
+            subject,
+            text: message
+        })
+    }
