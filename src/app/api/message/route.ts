48e5462023a20f0fc1f2510246dbf4d6a9393827
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-15 18:17:54 +0200

    REFINE: Consolidate request body parsing and enhance message sending logic

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,17 +1,27 @@
+import { sendMail } from "@/lib/email/sendMail";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     // Get the visitor's name, email and message from the request body
-    const { name } = await req.json();
-    const { email } = await req.json();
-    const { message } = await req.json();
+    const { name, email, message } = await req.json();
 
     // Send the message
-    // TODO: Add message-sending logic
+    await sendMail({
+      to: process.env.EMAIL || "",
+      subject: `New Message received from ${name}`,
+      message: `A new message was sent by ${name} via the form on our website. The message says: "${name}"`,
+    });
 
     // Send message to the visitor's email address
-    // TODO: Add message-sending logic
+    await sendMail({
+      to: email,
+      subject: `Hello ${name}, from Smart Switch Mobile`,
+      message:
+        "Hey there " +
+        name +
+        " , thanks for reaching out. Our team will get back to you in up to 12 hours. Thanks for making the Smart Switch",
+    });
 
     return NextResponse.json({
       message: "Message sent successfully",
