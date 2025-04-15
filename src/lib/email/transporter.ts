4f2576155f20f35bb5a01eb8e950ac2d4524aecb
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-15 18:16:40 +0200

    REFINE: Update nodemailer transporter configuration for Gmail

--- src/lib/email/transporter.ts
+++ src/lib/email/transporter.ts
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASS,
   },
 });
