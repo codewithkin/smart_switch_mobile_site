559c6ba8fa9cf331f374fbceee09d63a57c85449
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-17 14:57:35 +0200

    Refactor CartItem: replace div elements with semantic article tags and enhance animation with motion component

--- src/app/cart/components/CartItem.tsx
+++ src/app/cart/components/CartItem.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { useCartStore } from "@/stores/useCartStore"; // Adjust path
 import { Product } from "@/types"; // Adjust path
 import { FaTrashAlt } from "react-icons/fa"; // Icon for delete
+import { motion } from "framer-motion";
 
 interface CartItemProps {
   item: Product & { quantity: number };
@@ -10,32 +14,29 @@ function CartItem({ item }: CartItemProps) {
   const { removeFromCart } = useCartStore(); // Get the remove function from the store
 
   return (
-    <div className="flex items-center justify-between py-4 border-b">
-      <div className="flex items-center">
+    <motion.article className="flex items-center justify-between py-4 border-b">
+      <article className="flex items-center">
         <img
           src={item.images[0]} // Display the first image from the array (adjust if necessary)
           alt={item.name}
           className="w-16 h-16 object-cover mr-4"
         />
-        <div>
+        <article>
           <h3 className="font-semibold">{item.name}</h3>
           <p>
             {item.storage} | {item.color}
           </p>
           <p className="text-gray-600">x{item.quantity}</p>
-        </div>
-      </div>
+        </article>
+      </article>
 
-      <div className="flex items-center">
+      <article className="flex items-center gap-4">
         <p className="font-semibold">${item.price * item.quantity}</p>
-        <button
-          onClick={() => removeFromCart(item.id)}
-          className="ml-4 text-red-500 hover:text-red-700"
-        >
+        <Button variant="destructive" onClick={() => removeFromCart(item.id)}>
           <FaTrashAlt size={20} />
-        </button>
-      </div>
-    </div>
+        </Button>
+      </article>
+    </motion.article>
   );
 }
 
